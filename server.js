3f9fe3c8b5bcbf322b584a3caa72140e40232897
Saurabh Khartode <96560197+SAURABH-KHARTODE123@users.noreply.github.com>
2023-10-04 17:00:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ app.get("/",(req, res)=>{res.send("Welcome to  IET");});
 app.get("/product",(req, res)=>{var p={ "id":45, "title":"Gerbera","description":"Wedding Flower", "unitprice":14, "quatity":7000 };
                                        res.send(p);
 });
-app.listen(8000);
+app.listen(2300);
 console.log("App is listening on port 8000");
