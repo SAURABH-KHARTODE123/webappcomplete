121783b8a4b379b8f6dd5b2e5ceff31df74a5ef2
Saurabh Khartode <96560197+SAURABH-KHARTODE123@users.noreply.github.com>
2023-10-04 18:00:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var express=require('express');
 var app=express();
 app.use(express.static('public'))
 app.get("/",(req, res)=>{res.send("Welcome to  IET");});
-app.listen(2300);
+app.listen(9000);
 console.log("App is listening on port 2300");
